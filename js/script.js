25a17e87a22bc121495e42a190b8314e7b8c959a
Your Name <youremail@example.com>
2023-12-13 12:41:59 +0500

    learning

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-function addUpTo(n){
-    return n * (n + 1) / 2;
+function addUpTo(n) {
+
+    let total = 0;
+    for (let i = 1; i <=n; i++){
+        total += i;
+    }
+    
+    return total;
 }
 
-console.log(addUpTo(6));
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elaspsed: ${(t2 - t1)} / 1000} seconds.`)
