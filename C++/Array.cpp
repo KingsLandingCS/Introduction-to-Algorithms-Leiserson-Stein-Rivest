52a29f74c13d8c3a43b6c53b5f83ccd65bd63771
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2026-02-19 23:55:58 +0500

    learning

--- C++/Array.cpp
+++ C++/Array.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main()
 {
-    int arr[5];
+    int arr[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " " << endl;
+    }
 
-    
     return 0;
 }
