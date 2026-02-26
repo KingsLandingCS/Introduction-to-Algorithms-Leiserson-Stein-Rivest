9fad1410088734380edf8ab55339d8b337a504a9
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2026-02-26 21:32:55 +0500

    learning

---
+++ C++/secondLargestElementInArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+
+    int ans = INT_MIN;
+    // Largest element
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > ans)
+        {
+            ans = arr[i];
+        }
+    }
+
+    int second = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != ans)
+        {
+            second = max(second, arr[i]);
+        }
+    };
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Largest element: " << ans << endl;
+    cout << "Second largest: " << second << endl;
+
+    return 0;
+}
